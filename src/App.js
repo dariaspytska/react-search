cee93a1e4e875072b4b8c69c1d834b266b2fe350
dariaspytska <105360871+dariaspytska@users.noreply.github.com>
2022-08-08 01:04:30 +0300

    h1 fix

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Weather app</p>
+        <h1>Weather app</h1>
 
         <a
           className="App-link"
